79a9e9337effe25b7d5760702fc926810f2196c0
Totto16 <tobiausgais@gmail.com>
2023-08-02 22:13:49 +0200

    update fix chapter utils script

--- ffmpeg/fix-chapters.py
+++ ffmpeg/fix-chapters.py
@@ -9,30 +9,39 @@ LIST_TO_PROCESS: list[str] = [
 
 def main() -> None:
     for to_process in LIST_TO_PROCESS:
-        input = Path(to_process)
+        try:
+            input_file = Path(to_process)
 
-        output: Path = input.parent / (input.stem + "_output" + input.suffix)
+            output: Path = input_file.parent / (
+                input_file.stem + "_output" + input_file.suffix
+            )
 
-        launch_args: list[str] = [
-            "ffmpeg",
-            "-i",
-            str(input),
-            "-vcodec",
-            "copy",
-            "-acodec",
-            "copy",
-            "-map_chapters",
-            "-1",
-            "-y",
-            str(output),
-        ]
+            launch_args: list[str] = [
+                "ffmpeg",
+                "-i",
+                str(input_file),
+                "-vcodec",
+                "copy",
+                "-acodec",
+                "copy",
+                "-map_chapters",
+                "-1",
+                "-y",
+                str(output),
+            ]
 
-        subprocess.call(launch_args)
+            subprocess.call(launch_args)
 
-        temp_result = Path(__file__).parent / input.name
+            temp_result = (
+                Path(__file__).parent
+                / (input_file.parent.parent.name)
+                / input_file.name
+            )
 
-        shutil.move(input, temp_result)
-        shutil.move(output, input)
+            shutil.move(input_file, temp_result)
+            shutil.move(output, input_file)
+        except Exception:  # noqa: S110, BLE001
+            pass
 
 
 if __name__ == "__main__":
