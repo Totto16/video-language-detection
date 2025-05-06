f3824aed93571a4a40b78e7f0f50d5928ad309b6
Totto16 <tobiausgais@gmail.com>
2025-05-06 18:45:50 +0200

    feat: implement the InteractiveLanguagePicker with the help of another library
    it is still WIP, but the skeleton stands

--- src/content/language_picker.py
+++ src/content/language_picker.py
@@ -1,13 +1,22 @@
+import platform
+import subprocess
 from dataclasses import dataclass
+from enum import Enum
+from logging import Logger
 from pathlib import Path
-from typing import Annotated, Literal, Optional, Self, override
+from typing import Annotated, Literal, Optional, Self, TypedDict, cast, override
+
+from questionary import Choice, Question, Separator, select
+from questionary.prompts.common import FormattedText
 
 from content.general import (
     MissingOverrideError,
     OneOf,
 )
 from content.language import Language
-from content.prediction import Prediction
+from content.prediction import Prediction, PredictionBest
+from helper.log import get_logger
+from helper.terminal import Terminal
 
 
 class LanguagePicker:
@@ -39,11 +48,146 @@ class NoLanguagePicker(LanguagePicker):
         return None
 
 
+# TODO: is there a better way?
+class InteractiveLanguagePickerDict(TypedDict, total=False):
+    entries_to_show: int
+    show_full_list: bool
+
+
+class InteractiveLanguagePickerDictTotal(TypedDict, total=True):
+    entries_to_show: int
+    show_full_list: bool
+
+
+@dataclass()
+class PredictionBestSelectResult:
+    select_result_type: Literal["prediction_best"]
+    value: PredictionBest
+
+
+class SelectedType(Enum):
+    open = "open"
+    no_language = "no_language"
+
+
+@dataclass()
+class ManualSelectResult:
+    select_result_type: Literal["manual"]
+    selected: SelectedType
+
+
+SelectResult = PredictionBestSelectResult | ManualSelectResult
+
+
+def construct_choice(title: FormattedText, value: SelectResult) -> Choice:
+    return Choice(title=title, value=value)
+
+
+def ask_question(question: Question) -> Optional[SelectResult]:
+    return question.ask()
+
+
+def open_file(path: Path) -> None:
+    if platform.system() == "Darwin":  # macOS
+        subprocess.run(["open", path], check=True)  # noqa: S603, S607
+    elif platform.system() == "Windows":
+        subprocess.run(  # noqa: S602
+            ["cmd", "/c", "start", "", path],  # noqa: S607
+            shell=True,
+            check=True,
+        )
+    elif platform.system() == "Linux":
+        subprocess.run(["xdg-open", path], check=True)  # noqa: S603, S607
+    else:
+        msg = "Unsupported operating system"
+        raise OSError(msg)
+
+
+logger: Logger = get_logger()
+
+
 class InteractiveLanguagePicker(LanguagePicker):
+    __entries_to_show: int
+    __show_full_list: bool
+
     def __init__(
         self: Self,
+        *,
+        config: Optional[InteractiveLanguagePickerDict] = None,
     ) -> None:
         super().__init__()
+
+        loaded_dict: Optional[InteractiveLanguagePickerDict] = config
+        if loaded_dict is not None:
+            self.__entries_to_show = loaded_dict.get(
+                "entries_to_show",
+                self.__defaults["entries_to_show"],
+            )
+
+            self.__show_full_list = loaded_dict.get(
+                "show_full_list",
+                self.__defaults["show_full_list"],
+            )
+        else:
+            self.__entries_to_show = self.__defaults["entries_to_show"]
+            self.__show_full_list = self.__defaults["show_full_list"]
+
+    @property
+    def __defaults(self: Self) -> InteractiveLanguagePickerDictTotal:
+        return {
+            "entries_to_show": 10,
+            "show_full_list": False,
+        }
+
+    def __get_choices(
+        self: Self,
+        path: Path,
+        prediction: Prediction,
+    ) -> list[Choice]:
+
+        def format_choice(index: int, value: PredictionBest) -> Choice:
+            title: FormattedText = [
+                ("fg:ansiblue", f"[{index}]"),
+                ("", " "),
+                ("", f"{value.language}"),
+                ("", " - "),
+                ("fg:ansigreen", f"{value.accuracy:.2%}"),
+            ]
+
+            return construct_choice(
+                title=title,
+                value=PredictionBestSelectResult("prediction_best", value),
+            )
+
+        best_list = prediction.get_best_list()
+
+        items_count: int = (
+            len(best_list) if self.__show_full_list else self.__entries_to_show
+        )
+
+        result: list[Choice] = [
+            format_choice(i, best)
+            for i, best in enumerate(best_list)
+            if i < items_count
+        ]
+
+        result.append(Separator())
+
+        result.append(
+            construct_choice(
+                title=f"[open] '[{path}]'",
+                value=ManualSelectResult("manual", SelectedType.open),
+            ),
+        )
+
+        result.append(
+            construct_choice(
+                title="[no language]",
+                value=ManualSelectResult("manual", SelectedType.no_language),
+            ),
+        )
+
+        return result
 
     @override
     def pick_language(
@@ -51,8 +195,37 @@ class InteractiveLanguagePicker(LanguagePicker):
         path: Path,
         prediction: Prediction,
     ) -> Optional[Language]:
-        # TODO
-        return None
+        Terminal.clear()
+
+        choices = self.__get_choices(path, prediction)
+
+        question = select(
+            "Select the desired option:",
+            choices=choices,
+            default=choices[0],
+        )
+
+        while True:
+            result = ask_question(question)
+            if result is None:
+                continue
+
+            match result.select_result_type:
+                case "manual":
+                    manual_value = cast(ManualSelectResult, result)
+                    match manual_value.selected:
+                        case SelectedType.open:
+                            try:
+                                open_file(path)
+                            except RuntimeError as err:
+                                msg: str = f"Couldn't open file '{path}':\n{err}"
+                                logger.warning(msg)
+                            # fall trough and run the loop again
+                        case SelectedType.no_language:
+                            return None
+                case "prediction_best":
+                    prediction_value = cast(PredictionBestSelectResult, result)
+                    return prediction_value.value.language
 
 
 @dataclass
@@ -63,6 +236,7 @@ class NoLanguagePickerConfig:
 @dataclass
 class InteractiveLanguagePickerConfig:
     picker_type: Literal["interactive"]
+    config: Optional[InteractiveLanguagePickerDict]
 
 
 LanguagePickerConfig = Annotated[
@@ -78,4 +252,6 @@ def get_picker_from_config(
         case "none":
             return NoLanguagePicker()
         case "interactive":
-            return InteractiveLanguagePicker()
+            return InteractiveLanguagePicker(
+                config=cast(InteractiveLanguagePickerConfig, config).config,
+            )
