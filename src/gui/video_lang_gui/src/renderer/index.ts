67949cc2174c0849247688ef75a2e3148ff8b685
Totto16 <tobiausgais@gmail.com>
2026-05-21 06:00:23 +0200

    remove old implementation

--- src/gui/video_lang_gui/src/renderer/index.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication, platformBrowser } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-
-bootstrapApplication(App, appConfig, {
-    platformRef: platformBrowser()
-}).catch((err) => console.error(err));
