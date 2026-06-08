eac859d00cdee85ce2b5e0292843bbc37bb38912
Totto16 <tobiausgais@gmail.com>
2026-06-08 22:53:38 +0200

    fix: use local files for temporary urls

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -94,17 +94,15 @@ def video_file_dict() -> dict[str, VideoFile]:
             "video/mp4",
         ),
         # separator
-        "file_example_MP4_480_1_5MG.mp4": VideoFileURL(
-            "https://file-examples.com/storage/fe63e83a686a22f89a101a3/2017/04/file_example_MP4_480_1_5MG.mp4",
-            "video/mp4",
+        "file_example_MP4_480_1_5MG.mp4": VideoFileLocal(
+            "file_example_MP4_480_1_5MG.mp4",
         ),
         "file_example_WEBM_480_900KB.webm": VideoFileURL(
             "https://file-examples.com/wp-content/storage/2020/03/file_example_WEBM_480_900KB.webm",
             "video/mp4",
         ),
-        "file_example_AVI_480_750kB.avi": VideoFileURL(
-            "https://file-examples.com/storage/fe2c4db53a6a2330da0d375/2018/04/file_example_AVI_480_750kB.avi",
-            "video/mp4",
+        "file_example_AVI_480_750kB.avi": VideoFileLocal(
+            "file_example_AVI_480_750kB.avi",
         ),
         "file_example_MOV_480_700kB.mov": VideoFileURL(
             "https://file-examples.com/wp-content/storage/2018/04/file_example_MOV_480_700kB.mov",
