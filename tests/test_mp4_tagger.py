1287695edf1c1ea71aefce960b600ead2262113d
Totto16 <tobiausgais@gmail.com>
2026-06-09 00:06:33 +0200

    feat: add the correct mp4 mutagen tagger tests

--- tests/test_mp4_tagger.py
+++ tests/test_mp4_tagger.py
@@ -491,8 +491,51 @@ def test_mp4_tagger_metadata_tags_mutagen(
 
                 with tagger.writer(manager=test_manager) as w:
                     early_tags = w.get_tags()
+
+                    assert early_tags.uuid is None, "uuid can't be found yet"
+                    assert [
+                        *early_tags.metadata.items(),
+                    ] == [], "no metadata tags can be found already"
+
                     w.write_tags(tags)
 
                     next_tags = w.get_tags()
 
-                    assert early_tags == next_tags
+                    assert next_tags.uuid == tags.uuid, "UUID was written correctly"
+                    assert (
+                        next_tags.comment == tags.comment
+                    ), "Comment was written correctly"
+                    assert (
+                        next_tags.unrecognized == early_tags.unrecognized
+                    ), "no new unrecognized tags"
+                    assert (
+                        next_tags.metadata == tags.metadata
+                    ), "Metadata was written correctly"
+
+                    # write again, test that the uuid doesn't get overwritten
+
+                    new_tags = MetadataTags(
+                        comment=tags.comment,
+                        uuid=uuid4(),
+                        language=tags.language,
+                        metadata=tags.metadata,
+                    )
+
+                    assert new_tags.uuid != tags.uuid, "UUID should be unique"
+
+                    w.write_tags(new_tags)
+
+                    write_again_tags = w.get_tags()
+
+                    assert (
+                        next_tags.uuid == write_again_tags.uuid
+                    ), "UUID was not overwritten"
+                    assert (
+                        next_tags.comment == write_again_tags.comment
+                    ), "Comment was written correctly"
+                    assert (
+                        next_tags.unrecognized == write_again_tags.unrecognized
+                    ), "no new unrecognized tags"
+                    assert (
+                        next_tags.metadata == write_again_tags.metadata
+                    ), "Metadata was written correctly"
